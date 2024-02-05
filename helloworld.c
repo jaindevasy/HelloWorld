2f3bcccdea2882b7aadff6f81de03f9f31413a3e
Jain <jain.ad@trenser.com>
2024-02-05 14:51:59 +0530

    Helloworld for demo

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+   // printf() displays the string inside quotation
+	printf("Hello, World!\r\n");
+   
+   while(1)
+   {
+	   sleep(1);
+   }
+   
+   return 0;
+}
